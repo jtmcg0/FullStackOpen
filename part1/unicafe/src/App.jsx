08ad253fd441a93731ab561041561970b15b8e77
jtmcg0 <jtmcg@pm.me>
2024-08-29 15:44:22 +0000

    End of Exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,6 +7,22 @@ const Button = ({ handleClick, text}) => (
   </button>
 )
 
+const Statistics = (props) => {
+  const {good, neutral, bad, totalClicks} = props
+    return(
+      <div>
+        <h2>Current Feedback Statistics:</h2>
+        Good: {good} Neutral: {neutral} Bad: {bad}
+
+        <h2>Average Rating from -1 to 1:</h2>
+        {(good + (bad * -1)) / totalClicks}
+
+        <h2>Percentage of positive feedback:</h2>
+        {good / totalClicks * 100}%
+      </div>
+    )
+
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -43,14 +59,10 @@ const App = () => {
       &nbsp;
       <Button handleClick={handleBadClick} text='Bad'/>
       
-      <h2>Current Feedback Statistics:</h2>
-      Good: {good} Neutral: {neutral} Bad: {bad}
-
-      <h2>Average Rating from -1 to 1:</h2>
-      {(good + (bad * -1)) / totalClicks}
-
-      <h2>Percentage of positive feedback:</h2>
-      {good / totalClicks * 100}%
+      <Statistics good = {good}
+                  neutral = {neutral}
+                  bad = {bad}
+                  totalClicks = {totalClicks} />
     </div>
   )
 }
