b9fa0a707d62aafc89ef103a0e8dbb406e6e0f95
jtmcg0 <jtmcg@pm.me>
2024-08-29 15:33:58 +0000

    End of Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,31 +1,37 @@
 import { useState } from 'react'
 
 const Button = ({ handleClick, text}) => (
+  
   <button onClick={handleClick}>
     {text}
   </button>
 )
+
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [totalClicks, setTotal] = useState(0)
 
   const handleGoodClick = () => {
     console.log('UpdatedGood')
     const updatedGood = good + 1
     setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
   }
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
+    setTotal(updatedNeutral + good + bad)
   }
 
   const handleBadClick = () => {
     const updatedBad = bad + 1
     setBad(updatedBad)
+    setTotal(updatedBad + good + neutral)
   }
 
   return (
@@ -39,6 +45,12 @@ const App = () => {
       
       <h2>Current Feedback Statistics:</h2>
       Good: {good} Neutral: {neutral} Bad: {bad}
+
+      <h2>Average Rating from -1 to 1:</h2>
+      {(good + (bad * -1)) / totalClicks}
+
+      <h2>Percentage of positive feedback:</h2>
+      {good / totalClicks * 100}%
     </div>
   )
 }
