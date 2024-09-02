248099ea76ef596e1b3763b5b898e4e6c90532cb
jtmcg0 <jtmcg@pm.me>
2024-09-02 20:21:20 +0000

    End of Exercise 2.2

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -5,6 +5,7 @@ const Course = ({ course }) => {
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
 
     </div>
   )
@@ -31,19 +32,24 @@ const Part = (props) => {
 
 }
 
+
 const Content = ({parts}) => {
 
   return (
     parts.map((part) => 
     <Part key = {part.id} part = {part.name} exercises = {part.exercises} />
+    )  
   )
+}
+
+const Total = ({parts}) => {
+
+  const totalExercises = parts.reduce((total, part) => total + part.exercises, 0)
   
-//  return (
-//    <div>
-//      <Part part = {props.parts[0].name} exercises = {props.parts[0].exercises} />
-//      <Part part = {props.parts[1].name} exercises = {props.parts[1].exercises} />
-//      <Part part = {props.parts[2].name} exercises = {props.parts[2].exercises} />
-//    </div>
+  return (
+    <div>
+      Total exercises in course: {totalExercises}
+    </div>
   )
 }
 
